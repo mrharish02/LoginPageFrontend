08a8d203e51f37edb81212966de1caf811ce973c
mrharish02 <108951378+mrharish02@users.noreply.github.com>
2023-11-14 02:28:31 +0530

    Update login.js

--- src/login/login.js
+++ src/login/login.js
@@ -26,7 +26,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:3001/', {
+      const response = await fetch('https://loginpagebackend-sz4j.onrender.com/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
