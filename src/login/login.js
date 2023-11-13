054e6f517bf1de8a5d49ee8d7867c08961e3e36d
mrharish02 <108951378+mrharish02@users.noreply.github.com>
2023-11-14 02:15:34 +0530

    Update login.js

--- src/login/login.js
+++ src/login/login.js
@@ -7,93 +7,93 @@ const Login = () => {
   const [email,setEmail]=useState('')
   const [password,setPassword]=useState('')
   const navigate = useNavigate()
-  // async function userLogin(event) {
-  //   event.preventDefault();
-  //   if (!email && !password) {
-  //     console.log('Email and password cannot be empty.');
-  //     window.alert("Email and password cannot be empty!!!!!!!!");
-  //     return;
-  //   }
-  //   else if(!email){
-  //     console.log('Email cannot be empty.');
-  //     window.alert("Email cannot be empty!!!!!!!!");
-  //     return;
-  //   }
-  //   else if(!password){
-  //     console.log('Password cannot be empty.');
-  //     window.alert("Password cannot be empty!!!!!!!!");
-  //     return;
-  //   }
-
-  //   try {
-  //     const response = await fetch('http://localhost:3001/', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         email,
-  //         password,
-  //       }),
-  //       credentials: 'include',
-  //     });
-
-  //     console.log('login request sent');
-  //     const data = await response.json();
-  //     console.log(data);
-
-  //     if (data.status === 'success') {
-  //       // window.location.hre = '/home'
-  //       navigate('/home')
-  //     }
-  //     else if(data.status ==='invalid'){
-  //       window.alert('Wrong password!!!!')
-  //     }
-  //     else if(data.status === 'nouser'){
-  //       window.alert('Credentials not found!!!!!')
-  //     }
-  //   } catch (error) {
-  //     console.error('API Request Error:', error);
-  //   }
-  // }
-
   async function userLogin(event) {
     event.preventDefault();
-
     if (!email && !password) {
-      window.alert('Email and password cannot be empty!');
+      console.log('Email and password cannot be empty.');
+      window.alert("Email and password cannot be empty!!!!!!!!");
       return;
-    } else if (!email) {
-      window.alert('Email cannot be empty!');
+    }
+    else if(!email){
+      console.log('Email cannot be empty.');
+      window.alert("Email cannot be empty!!!!!!!!");
       return;
-    } else if (!password) {
-      window.alert('Password cannot be empty!');
+    }
+    else if(!password){
+      console.log('Password cannot be empty.');
+      window.alert("Password cannot be empty!!!!!!!!");
       return;
     }
 
     try {
-      const response = await axios.post('https://loginpagebackend-sz4j.onrender.com/', {
-        email,
-        password,
-      }, {
-        withCredentials: true, // Include credentials (cookies) with the request
+      const response = await fetch('http://localhost:3001/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+        credentials: 'include',
       });
 
       console.log('login request sent');
-      const data = response.data;
+      const data = await response.json();
       console.log(data);
 
       if (data.status === 'success') {
-        navigate('/home');
-      } else if (data.status === 'invalid') {
-        window.alert('Wrong password!');
-      } else if (data.status === 'nouser') {
-        window.alert('Credentials not found!');
+        // window.location.hre = '/home'
+        navigate('/home')
+      }
+      else if(data.status ==='invalid'){
+        window.alert('Wrong password!!!!')
+      }
+      else if(data.status === 'nouser'){
+        window.alert('Credentials not found!!!!!')
       }
     } catch (error) {
       console.error('API Request Error:', error);
     }
   }
+
+  // async function userLogin(event) {
+  //   event.preventDefault();
+
+  //   if (!email && !password) {
+  //     window.alert('Email and password cannot be empty!');
+  //     return;
+  //   } else if (!email) {
+  //     window.alert('Email cannot be empty!');
+  //     return;
+  //   } else if (!password) {
+  //     window.alert('Password cannot be empty!');
+  //     return;
+  //   }
+
+  //   try {
+  //     const response = await axios.post('https://loginpagebackend-sz4j.onrender.com/', {
+  //       email,
+  //       password,
+  //     }, {
+  //       withCredentials: true, // Include credentials (cookies) with the request
+  //     });
+
+  //     console.log('login request sent');
+  //     const data = response.data;
+  //     console.log(data);
+
+  //     if (data.status === 'success') {
+  //       navigate('/home');
+  //     } else if (data.status === 'invalid') {
+  //       window.alert('Wrong password!');
+  //     } else if (data.status === 'nouser') {
+  //       window.alert('Credentials not found!');
+  //     }
+  //   } catch (error) {
+  //     console.error('API Request Error:', error);
+  //   }
+  // }
 
   return (
     <>
